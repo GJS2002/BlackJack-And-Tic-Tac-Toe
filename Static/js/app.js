8d0a6793d3d9cd09c7601efb379e31d57499e9e4
GJS2002 <67513182+GJS2002@users.noreply.github.com>
2020-07-05 19:50:05 -0500

    Menu ready for mobile long view!

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,3 +1,16 @@
 const swup = new Swup();
 const botBtn = document.querySelector(".btn-bot");
 const playerBtn = document.querySelector(".btn-player");
+const btns = document.querySelectorAll("button");
+
+
+btns.forEach(btn => {
+     btn.addEventListener('click', e => {
+          let theBtn = e.currentTarget;
+          btns.forEach(btn => {btn.classList.remove('selected')});
+          theBtn.classList.add("selected");
+     })
+})
+
+
+
